5bf435601fb440e69e8908da3f1262b50718edb5
David Hurtado <thedavidhurtado@gmail.com>
2025-09-30 15:50:12 -0500

    Finishing up assignment.

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone(){
+        serialNumber = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String model) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        CellPhone myPhone;
+
+        myPhone = new CellPhone();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //classes have members: fields and methods
+// access modifiers: public, private,
+        //fields: class level variables
+        //encapsulate to make private or public, then use getter or setter
+       // access modifier return type, modifiers
+    //return type of setter: always void
+    //return type of getter: depends on what its getting
+
+
+
+
+}
