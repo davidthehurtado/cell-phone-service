53d394e359e103385bc604baeaf692d9f63a7e1e
David Hurtado <thedavidhurtado@gmail.com>
2025-10-01 15:53:04 -0500

    Excersise 3.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -7,51 +7,65 @@ public class CellPhone {
     private String phoneNumber;
     private String owner;
 
-    public CellPhone(){
+    public CellPhone() {
         serialNumber = 0;
         model = "";
         carrier = "";
         phoneNumber = "";
         owner = "";
     }
+    public CellPhone( int serialNumber, String model, String carrier, String phoneNumber, String owner){
+            this.serialNumber = serialNumber;
+            this.model = model;
+            this.carrier = carrier;
+            this.phoneNumber = phoneNumber;
+            this.owner = owner;
+        }
 
-    public int getSerialNumber() {
-        return serialNumber;
-    }
+        public int getSerialNumber () {
+            return serialNumber;
+        }
 
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
+        public void setSerialNumber ( int serialNumber){
+            this.serialNumber = serialNumber;
+        }
 
-    public String getModel() {
-        return model;
-    }
+        public String getModel () {
+            return model;
+        }
 
-    public void setModel(String n) {
-        this.model = n;
-    }
+        public void setModel (String n){
+            this.model = n;
+        }
 
-    public String getCarrier() {
-        return carrier;
-    }
+        public String getCarrier () {
+            return carrier;
+        }
 
-    public void setCarrier(String carrier) {
-        this.carrier = carrier;
-    }
+        public void setCarrier (String carrier){
+            this.carrier = carrier;
+        }
 
-    public String getPhoneNumber(){
-        return phoneNumber;
-    }
+        public String getPhoneNumber () {
+            return phoneNumber;
+        }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+        public void setPhoneNumber (String phoneNumber){
+            this.phoneNumber = phoneNumber;
+        }
 
-    public String getOwner() {
-        return owner;
-    }
+        public String getOwner () {
+            return owner;
+        }
 
-    public void setOwner(String owner) {
-        this.owner = owner;
+        public void setOwner (String owner){
+            this.owner = owner;
+        }
+
+        // DIAL METHOD
+        public void dial (String phoneNumber){
+            System.out.println(this.owner + "'s phone is calling " + phoneNumber);
+
+
+        }
     }
-}
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -3,13 +3,45 @@ package com.pluralsight;
 public class CellPhoneApplication {
     public static void main(String[] args) {
 
-        CellPhone myPhone;
-        myPhone = new CellPhone();
-        myPhone.setModel("iPhone 17");
-        myPhone.setPhoneNumber("902-565-5566");
+        CellPhone cellPhone1 = new CellPhone();
+        cellPhone1.setSerialNumber(4685699);
+        cellPhone1.setModel("iPhone 13");
+        cellPhone1.setCarrier("AT&T");
+        cellPhone1.setPhoneNumber("854-555-4555");
+        cellPhone1.setOwner("Glorilla Wyatt");
 
-        System.out.println(myPhone.getModel() + "\n" + myPhone.getPhoneNumber());
+        CellPhone cellPhone2 = new CellPhone();
+        cellPhone2.setSerialNumber(4211222);
+        cellPhone2.setModel("Samsung Galaxy");
+        cellPhone2.setCarrier("Verizon");
+        cellPhone2.setPhoneNumber("708-542-6685");
+        cellPhone2.setOwner("Beyonce Smith");
+
+        CellPhone cellPhone3 = new CellPhone(45622555, "Moto Razr", "T-Mobile", "456-785-3333", "Chris Hemsworth");
+
+        display(cellPhone1);
+        display(cellPhone2);
+        display(cellPhone3);
+
+
+        // PHONES CALLING EACH OTHER
+        cellPhone1.dial(cellPhone2.getPhoneNumber());
+        cellPhone2.dial(cellPhone1.getPhoneNumber());
+
+        cellPhone1.dial(cellPhone3.getPhoneNumber());
+        cellPhone3.dial(cellPhone1.getPhoneNumber());
     }
 
+    // DISPLAY
+    public static void display(CellPhone phone) {
+        System.out.println("Owner: " + phone.getOwner());
+        System.out.println("Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone Number: " + phone.getPhoneNumber());
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println();
     }
+}
+
+
 
